352a2fdda5e7c8c0525eff8e47f31654e8a92c3f
tamiltutera <tamiltutera@gmail.com>
2024-09-02 01:42:35 +0530

    add main.py and test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+from main import *
+
+def test_add():
+    assert add(2,3) == 5
+
+def test_subtract():
+    assert subtract(2,3) == -1
+
+def test_multiple():
+    assert multiple(2,3) == 6
+
+def test_divide():
+    assert divide(10,5) == 2
